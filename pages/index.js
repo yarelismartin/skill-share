f1939cf81dd04bfb1f181c70451304e6028c5146
Yarelis Martin <yarelis0martin@gmail.com>
2024-05-25 14:35:26 -0500

    completed setup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -16,7 +13,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
+      <h1>Welcome {user.displayName}! </h1>
+      <h4> We are thrilled to have you return to our community of knowledge sharers. Ready to pick up where you left off and explore even more skill-swapping opportunities?</h4>
     </div>
   );
 }
