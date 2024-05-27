7680937645ba7031fc0c1b6ef476065952536d4e
Yarelis Martin <yarelis0martin@gmail.com>
2024-05-27 18:33:14 -0500

    read and delete complete

--- components/forms/PostForm.js
+++ components/forms/PostForm.js
@@ -1,9 +1,70 @@
-import React from 'react';
+// import React, { useState } from 'react';
+// import { Form } from 'react-bootstrap';
 
-export default function PostForm() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  );
-}
+// export default function PostForm({ postObj }) {
+//   const [formInput, setFormInput] = useState();
+
+//   return (
+//     <div>
+//       <Form onSubmit={handleSubmit}>
+//         <h2>{postObj.firebaseKey ? 'Update' : 'Create'} Post</h2>
+
+//         {/* <Select Category/> */}
+//         <Form.Label>Category</Form.Label>
+//         <Form.Select
+//           name="category"
+//           value={formInput.category_id}
+//           onChange={handleChange}
+//           required="true"
+//         >
+//           <option value="" hidden>Choose a categpry</option>
+//           {selectCategory.map((category) => (
+//             <option
+//               key={category.firebaseKey}
+//               value={category.firebaseKey}
+//             >{category.name}
+//             </option>
+//           ))}
+
+//         </Form.Select>
+
+//         {/* TITLE INPUT  */}
+//         <Form.Group className="mb-3">
+//           <Form.Label>Title</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Post Title..."
+//             name="title"
+//             value={formInput.title}
+//             onChange={handleChange}
+//             required
+//           />
+//         </Form.Group>
+
+//         <Form.Group className="mb-3">
+//           <Form.Label>Position</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Player's Position..."
+//             name="position"
+//             value={formInput.position}
+//             onChange={handleChange}
+//             required
+//           />
+//         </Form.Group>
+//         <Form.Group className="mb-3">
+//           <Form.Label>Image</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Eneter a image address of player"
+//             name="image"
+//             value={formInput.image}
+//             onChange={handleChange}
+//             required
+//           />
+//         </Form.Group>
+//         <Button style={{ marginTop: '15px' }} type="submit">{memObj.firebaseKey ? 'Update' : 'Create'} Memeber</Button>
+//       </Form>
+//     </div>
+//   );
+// }
