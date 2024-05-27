5044ba82a5f110bfc7b5916296b948c946813bd0
Yarelis Martin <yarelis0martin@gmail.com>
2024-05-27 15:49:35 -0500

    created the card for posts

--- pages/community.js
+++ pages/community.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PostCard from '../components/PostCard';
 
 export default function community() {
   return (
     <div>
       <h1>Community</h1>
-
+      <PostCard />
     </div>
   );
 }
