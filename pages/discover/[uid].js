f4b428dcabd0f6913bbab82cdabff90270cbe383
Yarelis Martin <yarelis0martin@gmail.com>
2024-06-11 20:08:25 -0500

    tab is working

--- pages/discover/[uid].js
+++ pages/discover/[uid].js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ProfileTab from '../../components/ProfileTab';
+import Bio from '../../components/Bio';
+import Review from '../../components/Review';
 
 export default function ProfileDeatil() {
   return (
     <div>
-      <ProfileTab />
+      <ProfileTab>
+        <Bio label="Bio" />
+        <Review label="Review" />
+      </ProfileTab>
     </div>
   );
 }
