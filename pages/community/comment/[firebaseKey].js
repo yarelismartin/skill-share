a9da0452edf374a4e49138de2b1cfa5920824306
Yarelis Martin <yarelis0martin@gmail.com>
2024-06-17 20:44:52 -0500

    comments are visable on the post detail page

--- pages/community/comment/[firebaseKey].js
+++ pages/community/comment/[firebaseKey].js
@@ -3,26 +3,35 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { getSinglePost } from '../../../api/postData';
 import PostCard from '../../../components/PostCard';
-// import CommentCard from '../../../components/CommentCard';
+import CommentCard from '../../../components/CommentCard';
+import { getCommentsForUser } from '../../../api/commentData';
 
 export default function PostDetail() {
   const [post, setPost] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
-  // const [ comments, setComments ] = useState([])
+  const [comments, setComments] = useState([]);
 
   const singlePost = () => {
     getSinglePost(firebaseKey).then(setPost);
   };
 
+  const getComments = () => {
+    getCommentsForUser(firebaseKey).then(setComments);
+  };
+
   useEffect(() => {
+    getComments();
     singlePost();
   }, []);
 
   return (
     <div>
       <PostCard key={post.firebaseKey} postObj={post} />
-      {/* <CommentCard commentObj={}/> */}
+      {comments.map((comment) => (
+        <CommentCard key={comment.firebaseKey} commentObj={comment} />
+      ))}
+
     </div>
   );
 }
